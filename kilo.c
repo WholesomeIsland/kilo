d111783fcde825cad4f28f176f74b0b6b35f0c16
WholesomeIsland <65096556+WholesomeIsland@users.noreply.github.com>
2021-01-22 14:20:58 -0500

    SimpleCEditor

--- kilo.c
+++ kilo.c
@@ -162,21 +162,12 @@ void editorSetStatusMessage(const char *fmt, ...);
  * There is no support to highlight patterns currently. */
 
 /* C / C++ */
-char *C_HL_extensions[] = {".c",".h",".cpp",".hpp",".cc",NULL};
+char *C_HL_extensions[] = {".c",".h",".sc",NULL};
 char *C_HL_keywords[] = {
 	/* C Keywords */
 	"auto","break","case","continue","default","do","else","enum",
 	"extern","for","goto","if","register","return","sizeof","static",
 	"struct","switch","typedef","union","volatile","while","NULL",
-
-	/* C++ Keywords */
-	"alignas","alignof","and","and_eq","asm","bitand","bitor","class",
-	"compl","constexpr","const_cast","deltype","delete","dynamic_cast",
-	"explicit","export","false","friend","inline","mutable","namespace",
-	"new","noexcept","not","not_eq","nullptr","operator","or","or_eq",
-	"private","protected","public","reinterpret_cast","static_assert",
-	"static_cast","template","this","thread_local","throw","true","try",
-	"typeid","typename","virtual","xor","xor_eq",
 
 	/* C types */
         "int|","long|","double|","float|","char|","unsigned|","signed|",
